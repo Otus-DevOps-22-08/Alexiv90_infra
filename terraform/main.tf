dc320702b8a56dce7a0ad5aa59fcb25dbcd54f76
alex.ivanchenko <alex.ivanchenko@tele2.ru>
2023-02-14 12:30:20 +0300

    Add terraform-1 lesson

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  #token     = "y0_AgAAAABlUPNqAATuwQAAAADURcMs1EsJ3zd8SgaNEkb0j7-_anDggUM"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+resource "yandex_compute_instance" "app" {
+  name        = "reddit-app"
+  platform_id = "standard-v2"
+  resources {
+    core_fraction = 5
+    cores         = 2
+    memory        = 0.5
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  connection {
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+}
