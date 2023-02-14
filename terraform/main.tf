2d06ffd92fe57588a3bb935cb77bdaa0dc49062f
alex.ivanchenko <alex.ivanchenko@tele2.ru>
2023-02-14 12:50:11 +0300

    Add terraform-1 lesson

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  #token     = "y0_AgAAAABlUPNqAATuwQAAAADURcMs1EsJ3zd8SgaNEkb0j7-_anDggUM"
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.zone
