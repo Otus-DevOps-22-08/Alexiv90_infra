c365d029038d2c77d7f114d42ed415d949514dc8
alex.ivanchenko <alex.ivanchenko@tele2.ru>
2023-02-15 10:47:55 +0300

    Add terraform-1 lesson

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -2,9 +2,12 @@
 set -e
 APP_DIR=${1:-$HOME}
 sudo apt-get install -y git
+sleep 10
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
+sleep 10
 bundle install
+sleep 10
 sudo mv /tmp/puma.service /etc/systemd/system/puma.service
 sudo systemctl start puma
 sudo systemctl enable puma
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,14 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "yandex" {
-  #service_account_key_file = var.service_account_key_file
-  token     = "y0_AgAAAABlUPNqAATuwQAAAADURcMs1EsJ3zd8SgaNEkb0j7-_anDggUM"
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
   name        = "reddit-app"
   platform_id = "standard-v2"
+  zone        = var.zone
   resources {
     core_fraction = 5
     cores         = 2
@@ -30,23 +21,20 @@ resource "yandex_compute_instance" "app" {
 
   boot_disk {
     initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
       image_id = var.image_id
     }
   }
 
   network_interface {
-    # Указан id подсети default-ru-central1-a
     subnet_id = var.subnet_id
     nat       = true
   }
 
   connection {
-    type  = "ssh"
-    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    # путь до приватного ключа
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
     private_key = file(var.private_key)
   }
 
@@ -58,5 +46,4 @@ resource "yandex_compute_instance" "app" {
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
-
 }
